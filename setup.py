3c71e516c8ee7041f343ac79f58343cd1c643f9d
JohanObluda <tung.ts.fet.hust.k55@gmail.com>
2024-03-04 00:47:42 +0100

    - Package repo - Add readme - Add setup - Remove dummy test file

---
+++ setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup
+
+setup(
+    name="llm-transcript-generator",
+    packages=["llm-transcript-generator"],
+    version="1.0.0",
+    author="Tung Tran",
+    description="A LLM-based medical transcript generator",
+    long_description=open("README.md", "r", encoding="utf-8").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/tungsontran/llm-transcript-generator/",
+    license="MIT",
+    include_package_data=True,
+    install_requires=[],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.8',
+)
